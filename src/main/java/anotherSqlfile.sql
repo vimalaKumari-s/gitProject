87b83529422eac4a98fe1e303b55c6bc9671f363
vimalakumari <vimala.kumari@capillarytech.com>
2022-07-04 14:04:36 +0530

    validations

--- src/main/java/anotherSqlfile.sql
+++ src/main/java/anotherSqlfile.sql
@@ -1,4 +1,4 @@
 CREATE TABLE `emf_event_logs` (
-  `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `org_id` int(11) NOT NULL,
-  `event_name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL)
+  `id` int(11) ,
+  `org_id` int(11) ,
+  `event_name` varchar(32) )
