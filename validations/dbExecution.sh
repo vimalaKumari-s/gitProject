c28c48d9a5e07380574249d5551b4f801fc81378
vimalakumari <vimala.kumari@capillarytech.com>
2022-07-06 11:11:12 +0530

    validations

--- validations/dbExecution.sh
+++ validations/dbExecution.sh
@@ -1,4 +1,5 @@
 execute=$1
+ret=0
 if ( $execute ); then
   sudo /etc/init.d/mysql start
   mysql -e 'CREATE DATABASE test;' -uroot -proot
@@ -6,18 +7,12 @@ if ( $execute ); then
   mysql -e 'use test;' -uroot -proot
   for i in $2
      do
-     mysql -uroot -proot test < $i
+     mysql -uroot -proot test < $i || ret=1
      done
   for i in $3
      do
-     mysql -uroot -proot test < $i
+     mysql -uroot -proot test < $i || ret=1
      done
 fi
 
-echo "checking whether it prints the code"
-echo "$?" "status of previous command in file"
-if [[ ! ("$?" = "0") ]]; then
-  echo "it is returning the exit code other than 0"
-  exit 1
-fi
-exit 0
+exit $ret
