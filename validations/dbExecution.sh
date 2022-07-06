064e754e8b54f0fb9c2cb5bba892b7dd8b5a19da
vimalakumari <vimala.kumari@capillarytech.com>
2022-07-06 11:03:12 +0530

    validations

--- validations/dbExecution.sh
+++ validations/dbExecution.sh
@@ -10,12 +10,12 @@ if ( $execute ); then
      done
   for i in $3
      do
-     mysql -uroot -proot test < $i
+     mysql -uroot -proot test < $i || exit 1
      done
 fi
 
-if [[ ! ("$?" = "0") ]]; then
-  echo "it is returning the exit code other than 0"
-  exit 1
-fi
-exit 0
+#if [[ ! ("$?" = "0") ]]; then
+#  echo "it is returning the exit code other than 0"
+#  exit 1
+#fi
+#exit 0
